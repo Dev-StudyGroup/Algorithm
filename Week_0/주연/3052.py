2bf56d59ad146814c1896d185218997949d9867e
김주연 <keamjyn@gimjuyeon-ui-MacBookAir.local>
2022-09-19 17:32:11 +0900

    [Add] solve boj 3052 problem
    solve 3052 problem
    
    Signed-off-by: 김주연 <keamjyn@gimjuyeon-ui-MacBookAir.local>

--- Week_0/주연/3052.py
+++
@@ -1,8 +0,0 @@
-Python 3.10.6 (v3.10.6:9c7b4bd164, Aug  1 2022, 17:13:48) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
-arr=[]
-for i in range(10):
-    n=int(input())
-    arr.append(n%42)
-arr=set(arr)
-print(len(arr))
