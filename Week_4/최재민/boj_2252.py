dd64341accd1bb8bbc723ceef0c67ea584e62585
Choi jaemin <panzzang518@naver.com>
2022-10-14 15:36:32 +0900

    Solve: Week 4 advandced

---
+++ Week_4/최재민/boj_2252.py
@@ -0,0 +1,27 @@
+from collections import deque
+n,m = map(int, input().split())
+
+graph = [[] for _ in range(n+1)]
+inDegree = [0 for _ in range(n+1)]
+queue = deque()
+answer = []
+
+
+for i in range(m):
+  a, b = map(int, input().split())
+  graph[a].append(b)
+  inDegree[b] += 1
+
+for i in range(1, n+1):
+  if inDegree[i] == 0:
+    queue.append(i)
+
+while queue:
+  tmp = queue.popleft()
+  answer.append(tmp)
+  for i in graph[tmp]:
+    inDegree[i] -= 1
+    if inDegree[i] == 0:
+      queue.append(i)
+
+print(*answer)
